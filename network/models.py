364c09088d4228d1209643c0b483d2403e021311
Aditya Khera <adityakhera@Adityas-MacBook-Pro.local>
2024-07-29 15:07:43 +0100

    Added New Post Functionality

--- network/models.py
+++ network/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'posts_made')
+    content = models.CharField(max_length=280)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Post by {self.user.username} with id {self.id} on {self.timestamp}'
+
+class Like(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'liked_by')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name = 'likes', null=True, blank=True)
+
+    def __str__(self):
+        return f'Like by {self.user.username} on post {self.post.id}'
+
