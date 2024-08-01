3f64ab0dfee7c1c0a3f8b82bc1ab791117aca9df
Aditya Khera <adityakhera@Adityas-MacBook-Pro.local>
2024-08-01 18:41:01 +0100

    Added Pagination

--- network/urls.py
+++ network/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("<str:username>", views.profile, name='profile'),
-    path("follow/<str:username>", views.follow, name='follow')
+    path("profile/<str:username>", views.profile, name='profile'),
+    path("follow/<str:username>", views.follow, name='follow'),
+    path('following', views.followingpage, name='followingpage')
 ]
