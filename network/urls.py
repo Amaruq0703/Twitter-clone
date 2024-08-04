7a96fd69c68d1c16a8ef2242271ddc60853803ee
Amaruq0703 <aditya.khera7@gmail.com>
2024-08-04 19:47:33 +0100

    Added Post Editing Functionality

--- network/urls.py
+++ network/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("profile/<str:username>", views.profile, name='profile'),
     path("follow/<str:username>", views.follow, name='follow'),
-    path('following', views.followingpage, name='followingpage')
+    path('following', views.followingpage, name='followingpage'),
+    path('edit/<int:postId>', views.edit, name='edit')
 ]
